4b950022ce5c6301fded76e262b4ed1d1e187d9f
brandonlowon <64925339+brandonlowon@users.noreply.github.com>
2020-07-18 03:45:21 -0600

    Update foliumMap.py

--- foliumMap.py
+++ foliumMap.py
@@ -1,16 +1,19 @@
 import folium
 """
 foliumMap is a script that will draw out a map of the City of Calgary where the user can pass a latitude and longitude to pinpoint the area that has the highest traffic volume or incidents.
+
+Resources used: https://python-visualization.github.io/folium/quickstart.html
 """
 
+#
 m = folium.Map(
     location=[51.0447, -114.0719], #Coordinates of Calgary
-    zoom_start=12
-    
+    zoom_start=12   
 )
-51.048149, -114.037943
-def createMarker(latitude, longitude, name):
 
+def createMarker(latitude, longitude, name):
+    """createMarker will create and pin a marker on the map when given a latitude and longitiude, name arg creates a different version of the pin when the user mouse overs it
+    """
     if(name == "Traffic"):
         tooltip = 'Click me!'
         folium.Marker([latitude, longitude], popup='<i>Highest Volume Traffic</i>', tooltip=tooltip).add_to(m)
@@ -20,5 +23,4 @@ def createMarker(latitude, longitude, name):
 
 createMarker(51.048149, -114.037943, "Incident") #Coordinates of the highest Traffic flow from one CSV file
 
-
-m.save('map.html')
+m.save('map.html') #Creates a html file in my User Directory
