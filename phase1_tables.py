2cd5dbf3d96336480398b5f7f9f9f8774918aee1
lee38 <alee.western@gmail.com>
2020-07-20 21:49:36 -0600

    Update phase1_tables.py

--- phase1_tables.py
+++ phase1_tables.py
@@ -2,54 +2,119 @@ import pymongo
 import csv
 from pymongo import MongoClient
 import pandas as pd
-import numpy as np
-
-cluster = MongoClient(
-    "mongodb+srv://andrew:1234@cluster0.pgi1d.mongodb.net/CalgaryTraffic?ssl=true&ssl_cert_reqs=CERT_NONE")
-db = cluster["Test"]
-
-# LOAD TRAFFIC DATA
-df_volume_2016 = pd.read_csv("CSV/TrafficFlow2016_OpenData.csv")
-df_volume_2017 = pd.read_csv("CSV/2017_Traffic_Volume_Flow.csv")
-df_volume_2018 = pd.read_csv("CSV/Traffic_Volumes_for_2018.csv")
-
-# Convert to dictionary format for database upload
-df_volume_2016.reset_index(inplace = True)
-df_volume_2017.reset_index(inplace = True)
-df_volume_2018.reset_index(inplace = True)
-
-dict_volume_2016 = df_volume_2016.to_dict("records")
-dict_volume_2017 = df_volume_2017.to_dict("records")
-dict_volume_2018 = df_volume_2018.to_dict("records")
-
-# Upload to database
-db.TrafficVolume2016.insert_many(dict_volume_2016)
-db.TrafficVolume2017.insert_many(dict_volume_2017)
-db.TrafficVolume2018.insert_many(dict_volume_2018)
-
-# LOAD ACCIDENT DATA
-df_incidents = pd.read_csv("CSV/Traffic_Incidents.csv")
-df_incidents['START_DT'] = pd.to_datetime(df_incidents['START_DT'])
-
-# Split data into separate years
-accident_2016 = df_incidents.loc[df_incidents["START_DT"].dt.year == 2016, :]
-accident_2017 = df_incidents.loc[df_incidents["START_DT"].dt.year == 2017, :]
-accident_2018 = df_incidents.loc[df_incidents["START_DT"].dt.year == 2018, :]
-
-# Convert to dictionary format for database upload
-accident_2016.reset_index(inplace = True)
-dict_accident_2016 = accident_2016.to_dict("records")
-
-accident_2017.reset_index(inplace = True)
-dict_accident_2017 = accident_2017.to_dict("records")
-
-accident_2018.reset_index(inplace = True)
-dict_accident_2018 = accident_2018.to_dict("records")
-
-# Upload to database
-db.Accident2016.insert_many(dict_accident_2016)
-db.Accident2017.insert_many(dict_accident_2017)
-db.Accident2018.insert_many(dict_accident_2018)
+import pprint
+import dns
+import datetime
 
 
+def get_year_and_type():
+    '''gets the year and type selection from the combo boxes
+    '''
+    year = com_box_year.get()
+    type = com_box_type.get()
+    print(type, year)
+    return type, year
+
+# Read Steps:
+# 1. Call get_year_and_type and return year & type of desired dataset
+# 2. Find correct collection based off of return values
+# 3. Display data (accident vs traffic volume) and (year) [top 10 entries]
+# 4. Send status message
+
+# Sort Steps:
+# 1. Call get_year_and_type and return year & type of desired dataset
+# 2. Find correct collection based off of return values
+# 3. Display Data: Sort by traffic volume if type = traffic volume | Sort by accident if type=accident [top 10 entries]
+# 4. Send status message
+
+# Analysis Steps:
+# 1. Call get_year_and_type and return year & type of desired dataset
+# 2. Find correct collection based off of return values
+# 3. Find maximum values (sum i think) of traffic volume/accident for 2016, 2017, 2018
+# 4. Send status message
+
+# Map Steps:
+# 1. Call get_year_and_type and return year & type of desired dataset
+# 2. Find correct collection based off of return values
+# 3. Find section with max accidents/max traffic volume ???
+# 4. Display map with highlighted section
+# 5. Send status message
+
+
+def display_table(type, year):
+    '''reads combo box selection and displays table
+    '''
+    # type = get_year_and_type()[0]
+    # year = int(get_year_and_type()[1])
+
+    cluster = MongoClient(
+        "mongodb+srv://andrew:1234@cluster0.pgi1d.mongodb.net/CalgaryTraffic?ssl=true&ssl_cert_reqs=CERT_NONE")
+    db = cluster["Test"]
+
+    # Determine correct database-->collection
+
+    column_filter = {'_id': 0}
+    column_sort = {'secname': 1, 'volume': 1, '_id': 0}
+
+    table_read = 0
+    table_sort = 0
+    limit_count = 20
+
+    if type == "Traffic Volume":
+        if year == 2016:
+            collection = db.get_collection("TrafficVolume2016")
+
+            # Read Button Table - Alphabetical
+            table_read = collection.find({}, column_filter).sort("segment_name", 1).limit(limit_count)
+
+            # Sort Button Table - Volume Descending
+            table_sort = collection.find({}, column_filter).sort("volume", -1).limit(limit_count)
+
+        elif year == 2017:
+            collection = db.get_collection("TrafficVolume2017")
+            # Read Button Table - Alphabetical
+            table_read = collection.find({}, column_filter).sort("segment_name", 1).limit(limit_count)
+
+            # Sort Button Table - Volume Descending
+            table_sort = collection.find({}, column_filter).sort("volume", -1).limit(limit_count)
+
+        elif year == 2018:
+            collection = db.get_collection("TrafficVolume2018")
+            # Read Button Table - Alphabetical
+            table_read = collection.find({}, column_filter).sort("SECNAME", 1).limit(limit_count)
+
+            # Sort Button Table - Volume Descending
+            table_sort = collection.find({}, column_filter).sort("VOLUME", 1).limit(limit_count)
+
+    if type == "Accident":
+        if year == 2016:
+            collection = db.get_collection("Accident2016")
+            table_read = collection.find({}, {'_id': 0, 'index': 0}).sort("INCIDENT INFO", 1).limit(limit_count)
+            table_sort = collection.find({}, {'_id': 0, 'index': 0}).sort("Count", -1).limit(limit_count)
+
+        elif year == 2017:
+            collection = db.get_collection("Accident2017")
+            table_read = collection.find({}, {'_id': 0, 'index': 0}).sort("INCIDENT INFO", 1).limit(limit_count)
+            table_sort = collection.find({}, {'_id': 0, 'index': 0}).sort("Count", -1).limit(limit_count)
+
+        elif year == 2018:
+            collection = db.get_collection("Accident2018")
+            table_read = collection.find({}, {'_id': 0, 'index': 0}).sort("INCIDENT INFO", 1).limit(limit_count)
+            table_sort = collection.find({}, {'_id': 0, 'index': 0}).sort("Count", -1).limit(limit_count)
+
+    return table_read, table_sort
+
+
+table_read, table_sort = display_table("Traffic Volume", 2016)
+
+table_read = list(table_read)
+table_sort = list(table_sort)
+
+for item in table_read:
+    print(item)
+
+print("\n")
+
+for item in table_sort:
+    print(item)
 
